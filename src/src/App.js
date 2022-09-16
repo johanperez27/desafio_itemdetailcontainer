1138fc44a488f10611dae0949cc110b4fe833546
johanperez27 <90525356+johanperez27@users.noreply.github.com>
2022-09-15 20:58:19 -0500

    Add files via upload

---
+++ src/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ItemDetailContainer from './components/ItemDetailContainer.js/ItemDetailContainer'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ItemDetailContainer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
